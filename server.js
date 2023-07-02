526a6a14e6ad7bbcd84b8e96d4558466e5361fba
veronika24di <veronikanof909@gmail.com>
2023-07-02 14:44:20 +0300

    hw2-express

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-const app = require('./app')
+const app = require("./app.js");
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
+
+
 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  console.log(`Server running. Use our API on port: ${PORT}`);
+});
